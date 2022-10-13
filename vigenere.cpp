785c0bab4030da6f4a958ecac64d3c337a027c5a
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-13 16:39:13 -0400

    added working encryptVigenere function

--- vigenere.cpp
+++ vigenere.cpp
@@ -8,8 +8,46 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include "caesar.h"
 
 std::string encryptVigenere(std::string plaintext, std::string keyword)
 {
-    return "";
+    if(keyword.length() == 0)
+    {
+        return plaintext;
+    }
+
+    std::string s = "";
+    int numOfKey = 0;
+    int numLetters = 0;
+
+    std::string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    int keynums[keyword.length()] = { 0 };
+
+    for(int j=0; j<keyword.length(); j++)
+    {
+        for(int k=0; k<alpha.length(); k++)
+        {
+            if(toupper(keyword[j]) == alpha[k])
+            {
+                keynums[j] = k;
+            }
+        }
+    }
+    
+    for(int i=0; i<plaintext.length(); i++)
+    {
+        char newChar = plaintext[i];
+        if(isalpha(newChar))
+        {
+            numOfKey = numLetters % keyword.length();
+            numLetters++;
+            newChar = shiftChar(plaintext[i], keynums[numOfKey]);
+        }
+
+        s += newChar;
+    }
+
+    return s;
 }
