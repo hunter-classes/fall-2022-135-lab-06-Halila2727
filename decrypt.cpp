56b83f8f6c547a36f03d0aba437283dd96ff46eb
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-13 14:38:36 -0400

    added decryptCaesar and decryptVigenere functions but empty right now

---
+++ decrypt.cpp
@@ -0,0 +1,20 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Zamansky
+    Assignment: Lab 6D
+    This program will decrypt messages encrypted by Vigenere and Caesar ciphers.
+*/
+#include <iostream>
+#include <string>
+#include <cctype>
+
+std::string decryptCaesar(std::string ciphertext, int rshift)
+{
+    return "";
+}
+
+std::string decryptVigenere(std::string ciphertext, std::string keyword)
+{
+    return "";
+}
