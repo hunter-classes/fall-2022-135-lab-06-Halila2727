96ef8b8b563abbf54628eca4e0aa8be510c2dd8b
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-13 16:40:14 -0400

    created an output for encryptVigenere function

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@
   This program will call the functions made in other .cpp files
 */
 #include <iostream>
-
-
 #include "caesar.h"
+#include "vigenere.h"
 
 int main()
 {
   std::cout << "\nencryptCaesar() function:\nplaintext: Hello, World! Shift: 10\n";
   std::cout << encryptCaesar("Hello, World!", 10);
+  std::cout << "\n------------------\n";
+
+  std::cout << "\nencryptVigenere() function:\nplaintext: Hello, World! keyword: cake\n";
+  std::cout << encryptVigenere("Hello, World!", "cake");
   std::cout << "\n------------------\n";
   return 0;
 }
