de21b07d2b3d2aff5d6b549c55289d1a66592267
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-13 02:49:00 -0400

    created an output for encryptCaesar() function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
+/*
+  Author: Halil Akca
+  Course: CSCI-135
+  Instructor: Mike Zamansky
+  Assignment: Lab 05
+  This program will call the functions made in other .cpp files
+*/
 #include <iostream>
 
 
-#include "funcs.h"
+#include "caesar.h"
 
 int main()
 {
+  std::cout << "\nencryptCaesar() function:\nplaintext: Hello, World! Shift: 10\n";
+  std::cout << encryptCaesar("Hello, World!", 10);
+  std::cout << "\n------------------\n";
   return 0;
 }
