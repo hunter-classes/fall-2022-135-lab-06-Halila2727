f145d44c8733c7e13aadabc1c5c4e05307c493df
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-13 14:39:26 -0400

    added encryptVigenere function but empty right now

---
+++ vigenere.cpp
@@ -0,0 +1,15 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Zamansky
+    Assignment: Lab 6D
+    This program will implement the Vigenere cipher encryption.
+*/
+#include <iostream>
+#include <string>
+#include <cctype>
+
+std::string encryptVigenere(std::string plaintext, std::string keyword)
+{
+    return "";
+}
