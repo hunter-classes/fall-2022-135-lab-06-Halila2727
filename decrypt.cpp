c481ea79a41a537a4433d8d0908f858747e90c27
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-13 17:26:52 -0400

    created working decryptCaesar and decryptVigenere functions

--- decrypt.cpp
+++ decrypt.cpp
@@ -8,13 +8,71 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include "caesar.h"
 
 std::string decryptCaesar(std::string ciphertext, int rshift)
 {
-    return "";
+    int newShift = 0;
+
+    while (newShift < rshift)
+    {
+        newShift += 26;
+    }
+
+    newShift -= rshift;
+
+    std::string s = "";
+    for(int i=0; i<ciphertext.length(); i++)
+    {
+        char newChar = ciphertext[i];
+        if(isalpha(newChar))
+        {
+            newChar = shiftChar(ciphertext[i], newShift);
+        }
+        s += newChar;
+    }
+
+    return s;
 }
 
 std::string decryptVigenere(std::string ciphertext, std::string keyword)
 {
-    return "";
+    if(keyword.length() == 0)
+    {
+        return ciphertext;
+    }
+
+    std::string s = "";
+    int numOfKey = 0;
+    int numLetters = 0;
+
+    std::string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    int keynums[keyword.length()] = { 0 };
+
+    for(int j=0; j<keyword.length(); j++)
+    {
+        for(int k=0; k<alpha.length(); k++)
+        {
+            if(toupper(keyword[j]) == alpha[k])
+            {
+                keynums[j] = 26 - k;
+            }
+        }
+    }
+    
+    for(int i=0; i<ciphertext.length(); i++)
+    {
+        char newChar = ciphertext[i];
+        if(isalpha(newChar))
+        {
+            numOfKey = numLetters % keyword.length();
+            numLetters++;
+            newChar = shiftChar(ciphertext[i], keynums[numOfKey]);
+        }
+
+        s += newChar;
+    }
+
+    return s;
 }
