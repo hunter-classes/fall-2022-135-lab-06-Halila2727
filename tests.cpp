2670e096f22b1142a074cd8ded7e82cd2a5fe66c
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-13 16:41:12 -0400

    tested for cases of encryptViginere

--- tests.cpp
+++ tests.cpp
@@ -1,17 +1,29 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "caesar.h"
+#include "vigenere.h"
 
 // add your tests here
 TEST_CASE("Shift Character")
 {
 	CHECK(shiftChar('c', 26) == 'c');
     CHECK(shiftChar('c', 126) == 'y');
-    CHECK(shiftChar('z', 1) == 'a');
+    CHECK(shiftChar('z', -1) == 'y');
+    CHECK(shiftChar('z', 0) == 'z');
 }
 
 TEST_CASE("Encypt Caesar")
 {
 	CHECK(encryptCaesar("I'm a NYG and NYY fan!", 3) == "L'p d QBJ dqg QBB idq!");
     CHECK(encryptCaesar("I'm a NYG and NYY fan!", 126) == "E'i w JUC wjz JUU bwj!");
+    CHECK(encryptCaesar("I'm a NYG and NYY fan!", -1) == "H'l z MXF zmc MXX ezm!");
+    CHECK(encryptCaesar("I'm a NYG and NYY fan!", 0) == "I'm a NYG and NYY fan!");
+}
+
+TEST_CASE("Encypt Vigenere")
+{
+	CHECK(encryptVigenere("Candles can't break bones!", "cake") == "Eaxhnec gcn'd fteko doxiu!");
+    CHECK(encryptVigenere("Candles can't break bones!", "") == "Candles can't break bones!");
+    CHECK(encryptVigenere("Candles can't break bones!", "jbawebfpbq") == "Lbnzpfx rbd'c crael gdoub!");
+    CHECK(encryptVigenere("Candles can't break bones!", "tre mENd") == "Vrrdxif fte'x bdinn ufree!");
 }
