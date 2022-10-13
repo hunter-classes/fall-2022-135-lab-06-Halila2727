8ba264511f513da8fc6548eadd1f7ec6ac9e667a
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-13 03:07:13 -0400

    tested for cases of shiftChar and encryptCaesar

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,17 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "funcs.h"
+#include "caesar.h"
 
 // add your tests here
+TEST_CASE("Shift Character")
+{
+	CHECK(shiftChar('c', 26) == 'c');
+    CHECK(shiftChar('c', 126) == 'y');
+    CHECK(shiftChar('z', 1) == 'a');
+}
+
+TEST_CASE("Encypt Caesar")
+{
+	CHECK(encryptCaesar("I'm a NYG and NYY fan!", 3) == "L'p d QBJ dqg QBB idq!");
+    CHECK(encryptCaesar("I'm a NYG and NYY fan!", 126) == "E'i w JUC wjz JUU bwj!");
+}
